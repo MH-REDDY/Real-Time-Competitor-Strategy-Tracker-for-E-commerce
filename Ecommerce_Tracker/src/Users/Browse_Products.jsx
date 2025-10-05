54944654913ff830e7f6e0ed2c38e3d1a34ba395
kvpradeep279 <kollabcs136@gmail.com>
2025-10-05 12:25:21 +0530

    Skeleton of the project

---
+++ Ecommerce_Tracker/src/Users/Browse_Products.jsx
@@ -0,0 +1,18 @@
+// Swathi's Page
+// This page allows users to browse products available on the e-commerce platform.
+// It provides options to filter and sort products based on various criteria.
+// If filters seem too complex for u to make, a simple search bar wolud be enough.
+// Users can click on a product to view more details or add it to their cart.
+// What ever images you use keep them in public folder and use them here.
+import "./Browse_Products.css";
+
+function Browse_Products() {
+    return (
+        <div className="Browse_Products-container">
+            // start here
+
+            </div>
+    );
+}
+
+export default Browse_Products;
