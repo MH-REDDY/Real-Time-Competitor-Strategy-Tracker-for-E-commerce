673aeac8f3233b8d39f8076cafb2d5c716cd428f
kvpradeep279 <kollabcs136@gmail.com>
2025-10-05 12:25:21 +0530

    Skeleton of the project

---
+++ Ecommerce_Tracker/src/SignIn/User_SignUp.jsx
@@ -0,0 +1,17 @@
+// Dipika's Page
+// This page allows new users to create an account on the e-commerce platform.
+// It includes fields for entering Name, username, email, password, and other necessary details.
+// There is also an option to agree to terms and conditions(optional).
+// Upon successful sign-up, users are redirected to their homepage.
+import "./User_SignUp.css";
+
+function User_SignUp() {
+    return (
+        <div className="User_SignUp-container">
+            // start here
+
+            </div>
+    );
+}
+
+export default User_SignUp;
